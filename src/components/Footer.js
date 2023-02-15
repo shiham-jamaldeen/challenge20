59f87307158c401cd7e9c46331e17e68da99198f
shiham-jamaldeen <112249220+shiham-jamaldeen@users.noreply.github.com>
2023-02-15 22:53:35 +1030

    fixed small screen size css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer>
-      <ul>
+      <ul className="footerlink">
         <li className="footerlink">
           <i class="bi bi-github" style={{ fontSize: "60px" }}></i>
           <a
